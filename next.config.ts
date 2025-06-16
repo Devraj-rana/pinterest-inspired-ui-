509557a3258a55ef7f1bd1e378ec17dd362aca99
DEVRAJ RANA <devrajnara@gmail.com>
2025-06-16 14:25:08 +0530

    Refactor next.config.ts: Remove commented code and improve clarity of ESLint configuration. Refactor Client component: Update renderStars function to use TypeScript for rating parameter and enhance star rendering logic.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // ⚠️ This disables ESLint during builds
+
     ignoreDuringBuilds: true,
   },
 };
